999a85a51ef93537ded2d24c15525f26ae1acd8c
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2019-12-07 11:46:53 -0600

    Update turquoise.js

--- static-assets/turquoise.js
+++ static-assets/turquoise.js
@@ -1,11 +1,30 @@
 /* Turquoise scripts ========================================================== */
 /* 2019-12-07 John Earl Hardesty */
 
+function scrollToTop() {
+    $('html, body').animate({ scrollTop: 0 }, 400);
+    return false;
+  }
+  
+  function toggleScrollToTopButton() {
+    var threshold = 50;
+    if ($(window).scrollTop() > threshold) {
+      $('##return-to-top').fadeIn('slow');
+    } else {
+      $('#return-to-top').fadeOut('slow');
+    }
+  }
+  
+
 
 $( document ).ready(function() {
 
+    $('#return-to-top').click(scrollToTop);
+    $(window).scroll(toggleScrollToTopButton);
+
     /* return to top https://codepen.io/rdallaire/pen/apoyx ----------------------- */
     // ===== Scroll to Top ==== 
+    /*
     $(window).scroll(function() {
         if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
             $('#return-to-top').fadeIn(200);    // Fade in the arrow
@@ -17,6 +36,6 @@ $( document ).ready(function() {
         $('body').animate({
             scrollTop : 0                       // Scroll to top of body
         }, 500);
-    });
+    });*/
 
 });
