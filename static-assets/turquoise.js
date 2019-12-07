df54d03867b2f25ba11952c21ec69162b8c215a3
turquoise-trucking <57842675+turquoise-trucking@users.noreply.github.com>
2019-12-07 12:16:30 -0600

    Removed return to top
    Took too long figuring out how to hide properly from Safari iOS

--- static-assets/turquoise.js
+++ static-assets/turquoise.js
@@ -1,45 +1,16 @@
 /* Turquoise scripts ========================================================== */
 /* 2019-12-07 John Earl Hardesty */
 
-function scrollToTop() {
-    $('html, body').animate({ scrollTop: 0 }, 400);
-    return false;
-  }
-  
-  function toggleScrollToTopButton() {
-    var threshold = 50;
-    if ($(window).scrollTop() > threshold) {
-      $('#return-to-top').fadeIn('slow');
-    } else {
-      $('#return-to-top').fadeOut('slow');
-    }
-  }
+
   
 
 
 $( document ).ready(function() {
-
-    $('#return-to-top').click(scrollToTop);
-    $(window).scroll(toggleScrollToTopButton);
-
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+      })
     $('.popover-dismiss').popover({
         trigger: 'focus'
       })
-
-    /* return to top https://codepen.io/rdallaire/pen/apoyx ----------------------- */
-    // ===== Scroll to Top ==== 
-    /*
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-            $('#return-to-top').fadeIn(200);    // Fade in the arrow
-        } else {
-            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-        }
-    });
-    $('#return-to-top').click(function() {      // When arrow is clicked
-        $('body').animate({
-            scrollTop : 0                       // Scroll to top of body
-        }, 500);
-    });*/
 
 });
